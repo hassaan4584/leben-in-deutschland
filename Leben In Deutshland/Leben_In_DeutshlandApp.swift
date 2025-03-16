bc20a4a7f2c0cbe01a117a6cd0ae51acd2dc2e3c
Hassaan F. Ahmed <hassaan.fayyaz-ahmed@flixbus.com>
2025-03-16 16:18:06 +0100

    Initial Commit

---
+++ Leben In Deutshland/Leben_In_DeutshlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leben_In_DeutshlandApp.swift
+//  Leben In Deutshland
+//
+//  Created by Hassaan Ahmed on 16.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Leben_In_DeutshlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Leben In DeutshlandTests/Leben_In_DeutshlandTests.swift
@@ -0,0 +1,17 @@
+//
+//  Leben_In_DeutshlandTests.swift
+//  Leben In DeutshlandTests
+//
+//  Created by Hassaan Ahmed on 16.03.25.
+//
+
+import Testing
+@testable import Leben_In_Deutshland
+
+struct Leben_In_DeutshlandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
